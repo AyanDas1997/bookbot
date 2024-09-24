a77a29034cca21e67f05d8e77b62e4857d5e905e
AyanDas1997 <das.ayandas.ayan@gmail.com>
2024-09-24 22:55:29 +0530

    char count

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     count = count_words(text)
-    print(count)
+    new_char_dict = char_count(text)
+    print(new_char_dict)
 
 def get_book_text(file):
     with open(file) as f:
@@ -12,6 +13,14 @@ def count_words(text):
     words = text.split()
     return len(words)
 
-
+def char_count(text):
+    char_dict = {}
+    lowered_text = text.lower()
+    for char in lowered_text:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
 
 main()
